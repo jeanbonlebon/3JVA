2756885c9bd83598d66d012e124cd7e86614c3f1
Robin <w.robingross@gmail.com>
2017-02-22 09:54:55 +0100

    third commit

---
+++ js/load.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+
+
+
+      $('.dropdown-button').dropdown({
+      //hover: true, // Activate on hover
+      belowOrigin: true, // Displays dropdown below the button
+    }
+  );
+
+
+
+});
